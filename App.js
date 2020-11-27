4567c2bc47309e91ab2134877e7aac83ac6c5bbe
Hack-Light <onohsomtochukwubasil@gmail.com>
2020-11-27 01:40:17 +0100

    The play and stop button are working correctly

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 import Screen from "./components/Screen";
@@ -7,10 +7,42 @@ import AppButton from "./components/AppButton";
 export default function App() {
   console.log("App executed");
 
+  const [timer, setTimer] = useState(null);
+  const [bgColor, setBgColor] = useState("#fff");
+  const [playing, setPlaying] = useState(false);
+
+  const code_gen = () => {
+    let result = "#";
+    let code = Math.floor(Math.random() * 16777215).toString(16);
+    return result + code;
+  };
+
+  const changeColor = () => {
+    setBgColor(code_gen());
+  };
+
+  const handlePlay = () => {
+    setTimer(
+      setInterval(() => {
+        changeColor();
+      }, 500)
+    );
+  };
+
+  const handleStop = () => {
+    clearInterval(timer);
+    console.log("clicked");
+  };
+
   return (
-    <Screen style={styles.container}>
-      <AppButton title="Play" color="#5E72E4" />
-      <AppButton title="Stop" color="#fc5c65" />
+    <Screen style={[styles.container, { backgroundColor: bgColor }]}>
+      <AppButton title="Play" color="#5E72E4" onPress={handlePlay} />
+      <AppButton
+        title="Stop"
+        color="#fc5c65"
+        onPress={handleStop}
+        display={playing ? "block" : "none"}
+      />
       <AppButton title="Change" color="#11CDEF" />
     </Screen>
   );
@@ -19,7 +51,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+
     alignItems: "center",
     justifyContent: "center",
     padding: 5
