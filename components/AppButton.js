c6a07fbf7560438f4718a6b301fa7d1dde1ee343
Hack-Light <onohsomtochukwubasil@gmail.com>
2020-11-27 03:12:46 +0100

    Optimization Done

--- components/AppButton.js
+++ components/AppButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
-import COLORS from "../constants/Theme";
+
 
 export default function AppButton({
   title,
